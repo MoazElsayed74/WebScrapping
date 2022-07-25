5c75ab177fa04de3d6345f6ba5f404e3965dc02f
MoazElsayed74 <moazelsayed42@gmail.com>
2022-07-25 18:20:45 +0200

    last commit

--- index.js
+++ index.js
@@ -2,14 +2,32 @@
 const axios = require('axios')
 const cheerio = require('cheerio');
 const express = require('express')
-
-
 const app = express()
 const port = 3000
 
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
 
+const URL = 'https://www.theguardian.com/international'
+
+axios(URL)
+     .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        // create array to push the data to it 
+        const articles = []
+   // search by class name 
+        $('.fc-item__title',html).each(function()
+        {
+      const title =  $(this).text()
+      const URL = $(this).find('a').attr('href')
+      articles.push({
+        title,
+        URL
+      })
+     })
+console.log(articles)
+    }).catch(err => console.log(err))
+
+    // check the server is work 
 app.listen(port, () => console.log(`server running on PORT ${port}`))
